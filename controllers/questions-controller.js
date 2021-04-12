22361c837b7275abf17bfb8408178969e7613df9
sravanikumar <kumar.sra@northeastern.edu>
2021-04-12 01:21:39 -0400

    implemented findAllQuestions controller

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -2,11 +2,17 @@ const questionService = require("../services/questions/questions-service")
 
 module.exports = (app) => {
 
+    const findAllQuestions = (req, res) => {
+        const questions = questionService.findAllQuestions()
+        res.send(questions)
+    }
+
     const findQuestionsForQuiz = (req, res) => {
         const quizId = req.params['qid']
         const questions = questionService.findQuestionsForQuiz(quizId)
         res.json(questions)
     }
 
+    app.get("/api/questions", findAllQuestions);
     app.get("/api/quizzes/:qid/questions", findQuestionsForQuiz)
 }
