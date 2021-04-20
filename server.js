14a31093c6d99f41e36b27d75ad0f50100cde217
sravanikumar <kumar.sra@northeastern.edu>
2021-04-19 20:45:39 -0400

    updated server.js with quiz attempts controller

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(function (req, res, next) {
     next();
 });
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 require('./controllers/quizzes-controller')(app)
 require('./controllers/questions-controller')(app)
+require('./controllers/quiz-attempts-controller')(app)
 
 app.listen(3000)
