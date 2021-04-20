f42e7c4fe2eed7feda340e86ebfdea224063e686
sravanikumar <kumar.sra@northeastern.edu>
2021-04-20 03:34:04 -0400

    removed id for quiz attempts

--- models/quiz-attempts/quiz-attempts-schema.js
+++ models/quiz-attempts/quiz-attempts-schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const questionSchema = require('../questions/questions-schema')
 
 const quizAttempts = mongoose.Schema({
-    _id: String,
+    // _id: String,
     score: Number,
     quiz: {
         type: String,
