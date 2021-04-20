642335690755466316e5806ae56b632496c5ef83
sravanikumar <kumar.sra@northeastern.edu>
2021-04-19 23:52:39 -0400

    fixed port issue

--- server.js
+++ server.js
@@ -22,4 +22,4 @@ require('./controllers/quizzes-controller')(app)
 require('./controllers/questions-controller')(app)
 require('./controllers/quiz-attempts-controller')(app)
 
-app.listen(3000)
+app.listen(process.env.PORT || 3001)
