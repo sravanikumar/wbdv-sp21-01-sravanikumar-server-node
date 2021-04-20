4f562094272f4a6b56cee18bc266334baf4da602
sravanikumar <kumar.sra@northeastern.edu>
2021-04-19 20:19:13 -0400

    implemented questions DAO and refactored question service

---
+++ daos/questions-dao.js
@@ -0,0 +1,13 @@
+const questionsModel = require('../models/questions/questions-model')
+const quizzesModel = require('../models/quizzes/quizzes-model')
+
+const findAllQuestions = () => questionsModel.find()
+
+const findQuestionById = (qid) => questionsModel.findById(qid)
+
+const findQuestionsForQuiz = (qzid) => questionsModel.find({quizId: qzid})
+
+// const findQuestionsForQuiz = (qzid) => quizzesModel.findById(qzid)
+//     .populate('questions').then(quiz => quiz.questions)
+
+module.exports = { findAllQuestions, findQuestionById, findQuestionsForQuiz }
--- services/questions/questions-service.js
+++ services/questions/questions-service.js
@@ -1,27 +1,19 @@
-// let questions = require('./questions.json')
-const questionsModel = require("../../models/questions/questions-model")
+const questionsDao = require("../../daos/questions-dao")
 
 const createQuestion = () => {}
 
 const createQuestionForQuiz = () => {}
 
 const findAllQuestions = () => {
-    return questionsModel.find()
-    // return questions
+    return questionsDao.findAllQuestions()
 }
 
 findQuestionsForQuiz = (qzid) => {
-    return questionsModel.find({quizId: qzid})
-    // return questions.filter((question) => {
-    //     return question.quizId === qzid;
-    // })
+    return questionsDao.findQuestionsForQuiz(qzid)
 }
 
 const findQuestionById = (quid) => {
-    return questionsModel.findById(quid)
-    // return questions.find((question) => {
-    //     return question._id === quid;
-    // })
+    return questionsDao.findQuestionById(quid)
 }
 
 const updateQuestion = () => {}
